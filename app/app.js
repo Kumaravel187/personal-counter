93f453f7e2df5b38e752a9ffb7e2c315b94acc3a
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-29 20:44:26 +0800

    Updated javascript
    - Used foreach instead for btns
    - Added styling changes for negative and positive values

--- app/app.js
+++ app/app.js
@@ -1,24 +1,44 @@
+// Get the element that will display the counter value
 const counterValEl = document.getElementById("count");
-const decreaseEl = document.getElementById("decrease");
-const increaseEl = document.getElementById("increase");
-const resetEl = document.getElementById("reset");
 
-counterValEl.textContent = 0;
+// Get all buttons with class "btn"
+const btns = document.querySelectorAll(".btn");
 
-decreaseEl.addEventListener("click", function () {
-  let message = "Counter cannot be negative";
-  if (counterValEl.textContent <= 0) {
-    console.log("Cannot be lower than zero");
-    alert(message);
-  } else {
-    counterValEl.textContent--;
-  }
-});
+// Initialize the counter value
+let count = 0;
 
-increaseEl.addEventListener("click", function () {
-  counterValEl.textContent++;
-});
+// Loop through each button and attach a click event listener
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    // Check if the clicked button has the class "decrease"
+    if (btn.classList.contains("decrease")) {
+      // Decrease the counter value
+      count--;
+    }
+    // Check if the clicked button has the class "increase"
+    else if (btn.classList.contains("increase")) {
+      // Increase the counter value
+      count++;
+    }
+    // If the clicked button has neither "decrease" nor "increase" class
+    else {
+      // Reset the counter value to 0
+      count = 0;
+    }
 
-resetEl.addEventListener("click", function () {
-  counterValEl.textContent = 0;
+    // Change text color based on counter value
+    if (count < 0) {
+      // Set text color to red for negative values
+      counterValEl.style.color = "#F94144";
+    } else if (count > 0) {
+      // Set text color to green for positive values
+      counterValEl.style.color = "#90BE6D";
+    } else {
+      // Set text color to light yellow for zero
+      counterValEl.style.color = "#eaf8bf";
+    }
+
+    // Update the text content of the counter element to reflect the new value
+    counterValEl.textContent = count;
+  });
 });
